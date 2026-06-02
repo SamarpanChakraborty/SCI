0a755d9e2af9d0945a9e351d6befbb421a5d45b7
Samarpan Chakraborty <samarpan93@gmail.com>
2026-06-02 10:02:40 +0530

    Update .gitignore, modify image loading and training scripts, and add new model weights

--- CVPR/multi_read_data.py
+++ CVPR/multi_read_data.py
@@ -30,6 +30,10 @@ class MemoryFriendlyLoader(torch.utils.data.Dataset):
 
     def load_images_transform(self, file):
         im = Image.open(file).convert('RGB')
+        # Resize image to ensure it's at least batch_h x batch_w
+        if im.size[0] < batch_w or im.size[1] < batch_h:
+            im = im.resize((max(batch_w, im.size[0]), max(
+                batch_h, im.size[1])), Image.BILINEAR)
         img_norm = self.transform(im).numpy()
         img_norm = np.transpose(img_norm, (1, 2, 0))
         return img_norm
@@ -41,11 +45,11 @@ class MemoryFriendlyLoader(torch.utils.data.Dataset):
         h = low.shape[0]
         w = low.shape[1]
         #
-        h_offset = random.randint(0, max(0, h - batch_h - 1))
-        w_offset = random.randint(0, max(0, w - batch_w - 1))
+        h_offset = random.randint(0, max(0, h - batch_h))
+        w_offset = random.randint(0, max(0, w - batch_w))
         #
-        # if self.task != 'test':
-        #     low = low[h_offset:h_offset + batch_h, w_offset:w_offset + batch_w]
+        if self.task != 'test':
+            low = low[h_offset:h_offset + batch_h, w_offset:w_offset + batch_w]
 
         low = np.asarray(low, dtype=np.float32)
         low = np.transpose(low[:, :, :], (2, 0, 1))
--- TPAMI/train.py
+++ TPAMI/train.py
@@ -18,13 +18,15 @@ from multi_read_data import MemoryFriendlyLoader
 
 parser = argparse.ArgumentParser("SCI")
 parser.add_argument('--batch_size', type=int, default=1, help='batch size')
-parser.add_argument('--cuda', default=True, type=bool, help='Use CUDA to train model')
+parser.add_argument('--cuda', default=True, type=bool,
+                    help='Use CUDA to train model')
 parser.add_argument('--gpu', type=str, default='0', help='gpu device id')
 parser.add_argument('--seed', type=int, default=2, help='random seed')
 parser.add_argument('--epochs', type=int, default=3000, help='epochs')
 parser.add_argument('--lr', type=float, default=0.0003, help='learning rate')
 parser.add_argument('--stage', type=int, default=3, help='epochs')
-parser.add_argument('--save', type=str, default='Rebuttal/000/', help='location of the data corpus')
+parser.add_argument('--save', type=str, default='Rebuttal/000/',
+                    help='location of the data corpus')
 
 args = parser.parse_args()
 
@@ -61,7 +63,8 @@ else:
 def save_images(tensor, path):
     image_numpy = tensor[0].cpu().float().numpy()
     image_numpy = (np.transpose(image_numpy, (1, 2, 0)))
-    im = Image.fromarray(np.clip(image_numpy * 255.0, 0, 255.0).astype('uint8'))
+    im = Image.fromarray(
+        np.clip(image_numpy * 255.0, 0, 255.0).astype('uint8'))
     im.save(path, 'png')
 
 
@@ -81,41 +84,48 @@ def main():
     model = Network(stage=args.stage)
 
     model = model.cuda()
-    optimizer_a = torch.optim.Adam(model.ha.parameters(), lr=args.lr, betas=(0.9, 0.999), weight_decay=3e-4)
-    optimizer_b = torch.optim.Adam(list(model.hb.parameters()) + list(model.calibrate.parameters()), lr=args.lr, betas=(0.9, 0.999), weight_decay=3e-4)
+    optimizer_a = torch.optim.Adam(model.ha.parameters(
+    ), lr=args.lr, betas=(0.9, 0.999), weight_decay=3e-4)
+    optimizer_b = torch.optim.Adam(list(model.hb.parameters(
+    )) + list(model.calibrate.parameters()), lr=args.lr, betas=(0.9, 0.999), weight_decay=3e-4)
 
     MB = utils.count_parameters_in_MB(model)
     logging.info("model size = %f", MB)
     print(MB)
 
-
     train_low_data_names = 'Your training data path'
-    TrainDataset = MemoryFriendlyLoader(img_dir=train_low_data_names, task='train')
+    TrainDataset = MemoryFriendlyLoader(
+        img_dir=train_low_data_names, task='train')
 
     test_low_data_names = 'Your testing data path'
-    TestDataset = MemoryFriendlyLoader(img_dir=test_low_data_names, task='test')
+    TestDataset = MemoryFriendlyLoader(
+        img_dir=test_low_data_names, task='test')
 
     train_queue = torch.utils.data.DataLoader(
         TrainDataset, batch_size=args.batch_size,
-        num_workers=0, shuffle=False, generator=torch.Generator(device = 'cuda'))
+        num_workers=0, shuffle=False, generator=torch.Generator(device='cuda'))
 
     test_queue = torch.utils.data.DataLoader(
         TestDataset, batch_size=1,
-        num_workers=0, shuffle=False, generator=torch.Generator(device = 'cuda'))
+        num_workers=0, shuffle=False, generator=torch.Generator(device='cuda'))
 
     total_step = 0
     for epoch in range(args.epochs):
         model.train()
         losses = []
+        # loss1_list = []
+        # loss2_list = []
+        # loss3_list = []
+        epoch_start_time = time.time()
+
         for batch_idx, (input, _) in enumerate(train_queue):
-            
 
             total_step += 1
             input = Variable(input, requires_grad=True).cuda()
 
-            _, loss1 , loss2 , loss3  = model._loss_Jiaoti(input)
-            
-            if total_step %10 < 7:
+            _, loss1, loss2, loss3 = model._loss_Jiaoti(input)
+
+            if total_step % 10 < 7:
                 for param in model.ha.parameters():
                     param.requires_grad = True
                 for param in model.hb.parameters():
@@ -136,15 +146,32 @@ def main():
                     param.requires_grad = True
                 optimizer_b.zero_grad()
 
-                loss = (loss2 + loss3) 
+                loss = (loss2 + loss3)
                 loss.backward()
                 optimizer_b.step()
-            
+
             nn.utils.clip_grad_norm_(model.parameters(), 5)
-            
+
             losses.append(loss.item())
-            logging.info('train-epoch-{:0>3d}-step-{:0>5d}  Tloss:{:<8.4f} loss1:{:<8.4f} loss2:{:<8.4f} loss3:{:<8.4f}'\
-                         .format(epoch, batch_idx, loss,loss1,loss2,loss3))
+            # loss1_list.append(loss1.item())
+            # loss2_list.append(loss2.item())
+            # loss3_list.append(loss3.item())
+
+            logging.info('train-epoch-{:0>3d}-step-{:0>5d}  Tloss:{:<8.4f}'
+                         .format(epoch, batch_idx, loss))
+            # logging.info('train-epoch-{:0>3d}-step-{:0>5d}  Tloss:{:<8.4f} loss1:{:<8.4f} loss2:{:<8.4f} loss3:{:<8.4f}'
+            #              .format(epoch, batch_idx, loss, loss1, loss2, loss3))
+
+            # Iteration summary every 50 steps
+            if total_step % 50 == 0 and total_step != 0:
+                avg_loss_50 = np.average(losses[-50:])
+                # avg_loss1_50 = np.average(loss1_list[-50:])
+                # avg_loss2_50 = np.average(loss2_list[-50:])
+                # avg_loss3_50 = np.average(loss3_list[-50:])
+                logging.info('ITER SUMMARY: Epoch[{:0>3d}] Total_Step[{:>6d}] Avg_Loss(50iter):{:<8.4f}'
+                             .format(epoch, total_step, avg_loss_50))
+                # logging.info('ITER SUMMARY: Epoch[{:0>3d}] Total_Step[{:>6d}] Avg_Loss(50iter):{:<8.4f} L1:{:<8.4f} L2:{:<8.4f} L3:{:<8.4f}'
+                #              .format(epoch, total_step, avg_loss_50, avg_loss1_50, avg_loss2_50, avg_loss3_50))
 
             if total_step % 500 == 0 and total_step != 0:
                 logging.info('train %03d %f', epoch, loss)
@@ -153,15 +180,36 @@ def main():
                     for _, (input, image_name) in enumerate(test_queue):
                         input = Variable(input, volatile=True).cuda()
                         image_name = image_name[0].split('/')[-1].split('.')[0]
-                        _, ref_list, _, _= model(input)
+                        _, ref_list, _, _ = model(input)
                         for ii in range(1):
-                            u_name = '{}_{}_{}.png'.format(image_name, total_step, ii)
+                            u_name = '{}_{}_{}.png'.format(
+                                image_name, total_step, ii)
                             u_path = image_path + '/' + u_name
                             save_images(ref_list[ii], u_path)
                 model.train()
-                logging.info('train-epoch %03d %f', epoch, np.average(losses))
-                utils.save(model, os.path.join(model_path, 'weights_%d_%d.pt' % (epoch, total_step)))
+                avg_loss_epoch = np.average(losses)
+                logging.info('CHECKPOINT: Epoch[{:0>3d}] Total_Step[{:>6d}] Avg_Loss:{:<8.4f}'
+                             .format(epoch, total_step, avg_loss_epoch))
+                utils.save(model, os.path.join(
+                    model_path, 'weights_%d_%d.pt' % (epoch, total_step)))
 
+        # Epoch summary
+        epoch_time = time.time() - epoch_start_time
+        avg_loss_epoch = np.average(losses)
+        # avg_loss1_epoch = np.average(loss1_list)
+        # avg_loss2_epoch = np.average(loss2_list)
+        # avg_loss3_epoch = np.average(loss3_list)
+
+        logging.info('='*80)
+        logging.info('EPOCH SUMMARY: Epoch[{:0>3d}] | Time:{:.2f}s | Iterations:{:>5d} | Batches:{:>5d}'.format(
+            epoch, epoch_time, total_step, batch_idx+1))
+        logging.info('  Train Loss - Avg:{:<8.4f} | Min:{:<8.4f} | Max:{:<8.4f} | Std:{:<8.4f}'.format(
+            avg_loss_epoch, np.min(losses), np.max(losses), np.std(losses)))
+        # logging.info('  Train Loss - Total:{:<8.4f} | Loss1:{:<8.4f} | Loss2:{:<8.4f} | Loss3:{:<8.4f}'.format(
+        #     avg_loss_epoch, avg_loss1_epoch, avg_loss2_epoch, avg_loss3_epoch))
+        # logging.info('  Min Loss: {:<8.4f} | Max Loss: {:<8.4f} | Std Dev: {:<8.4f}'.format(
+        #     np.min(losses), np.max(losses), np.std(losses)))
+        logging.info('='*80)
 
 
 if __name__ == '__main__':
